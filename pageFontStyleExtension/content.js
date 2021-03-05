40d3248587f4c2209171ca15508a3ad43769c3ab
jandonggun <donggontrannguyen@gmail.com>
2021-03-05 08:54:20 +0700

    content.js comment

--- pageFontStyleExtension/content.js
+++ pageFontStyleExtension/content.js
@@ -1,1 +1,7 @@
 chrome.runtime.sendMessage({ todo: "showPageAction" });
+
+// content js có thể thay đổi dom, syle nhưng không thể sài
+// hầu hết chrome api như storage và notification
+// nên là content sẽ nhờ eventPage làm hộ nó bằng
+// cách gửi 1 thông điệp show page action
+// để hilight đc extension
