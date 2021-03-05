55b1732928a691064ed4ce28bde91a377d8cb20d
jandonggun <donggontrannguyen@gmail.com>
2021-03-05 10:11:33 +0700

    popup.js

--- pageFontStyleExtension/content.js
+++ pageFontStyleExtension/content.js
@@ -5,3 +5,10 @@ chrome.runtime.sendMessage({ todo: "showPageAction" });
 // nên là content sẽ nhờ eventPage làm hộ nó bằng
 // cách gửi 1 thông điệp show page action
 // để hilight đc extension
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.todo === "changeColor") {
+    const color = request.color;
+    document.querySelector(".type--h1").style.color = color;
+  }
+});
